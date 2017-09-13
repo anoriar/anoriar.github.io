7cfb4f64c3865e960f2ed97fc0a69bafcb417daf
anoriar <borisov.alex12@gmail.com>
2017-09-13 15:39:36 +0300

    build master landing v1.0

---
+++ build_master_landing/js/common.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+	//табы
+	$('ul.tabs_caption').each(function() {
+    $(this).find('li').each(function(i) {
+      $(this).click(function(){
+        $(this).addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs_content').removeClass('active').eq(i).addClass('active');
+      });
+    });
+  });
+
+	//слайдер
+  $(".owl-carousel").owlCarousel({
+		responsive : {
+			0 : {
+				items : 1,
+				nav : true
+			}
+		},
+		navText : ""
+	});
+
+  //параллакс
+  $.stellar({
+		responsive: true
+	});
+
+   $('.popup').magnificPopup();
+
+	//цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#form").submit(function(e) {
+		e.preventDefault;
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+	});
+	
+});
+
